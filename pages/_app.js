f8f98b1530b505d3955bbd8394a2398c0352295a
pollomax847 <votre.email@example.com>
2025-04-26 10:45:32 +0200

    fix: Correction des chemins d'import

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import '@/styles/globals.css'
-import '@/styles/theme.css'
-import '@/styles/notifications.css'
-import { ThemeProvider } from '@/components/ThemeProvider'
-import { NotificationProvider } from '@/components/Notification'
+import '../styles/globals.css'
+import '../styles/theme.css'
+import '../styles/notifications.css'
+import { ThemeProvider } from '../components/ThemeProvider'
+import { NotificationProvider } from '../components/Notification'
 import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
